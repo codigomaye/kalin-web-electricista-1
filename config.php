0dfc108a0ddbdd3e71bdd90d5b2e8a3afb27000c
David Maye <82540453+codigomaye@users.noreply.github.com>
2025-03-15 17:14:08 +0100

    Update site name

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Str;
 return [
     'baseUrl' => '',
     'production' => false,
-    'siteName' => 'Electricista Sierra',
+    'siteName' => 'Electricidad Galapagar',
     'siteDescription' => 'Generate an elegant blog with Jigsaw',
     'siteAuthor' => 'Author Name',
 
